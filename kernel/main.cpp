e858625a5d0b8f262d076febcf54400686886cde
Userlight <ldsnikola@gmail.com>
2026-02-17 08:12:32 -0300

    Create main.cpp

---
+++ kernel/main.cpp
@@ -0,0 +1,22 @@
+#include "../include/nigtype.hpp"
+#include "../include/nigtype.h"
+
+extern "C" void kernel_main() {
+    NigTypeOS os;
+    
+    // 1. CHUVA DE LETRAS (Boot Log)
+    os.clear_screen();
+    run_boot_logs(); // Função em C
+
+    // 2. TELA DE CONFIRMACAO (Instalador)
+    os.run_setup_wizard(); // Função em C++
+
+    // 3. ENTRADA NO SISTEMA REAL
+    os.clear_screen();
+    os.draw_taskbar(); // Sua barra grossa no topo
+    
+    os.print_at("NigType-OS pronto para uso!", 2, 2, 0x0F);
+    os.print_at("Clique no menu ou digite 'chrome' no terminal.", 2, 3, 0x07);
+
+    while(1); // Mantém o sistema vivo
+}
